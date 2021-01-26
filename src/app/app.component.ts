a90c3bb09e116bafa53de1b8d7742b8a3068d37e
Максим Рябец <maksaxel2@gmail.com>
2021-01-26 10:16:02 +0200

    feat(app): add dependencies for slider

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Image } from './image';
+import { SliderService } from './slider.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +9,19 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {}
+export class AppComponent {
+  slides$: Observable<Image[]> = this.sliderService.getSlides();
+
+  // [size screen, count slides]
+  breakpoints = new Map([
+    [576, 1],
+    [768, 2],
+    [992, 3],
+    [1199, 4],
+    [1200, 5],
+  ]);
+
+  delay = 5000;
+
+  constructor(private readonly sliderService: SliderService) {}
+}
