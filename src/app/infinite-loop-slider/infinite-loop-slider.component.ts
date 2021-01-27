2708eb202efdb864ab31f74e8596d4e885125508
Максим Рябец <maksaxel2@gmail.com>
2021-01-27 13:23:25 +0200

    docs(infinity-loop-slider): update comments for class

--- src/app/infinite-loop-slider/infinite-loop-slider.component.ts
+++ src/app/infinite-loop-slider/infinite-loop-slider.component.ts
@@ -20,7 +20,9 @@ import { isPlatformBrowser } from '@angular/common';
 })
 
 /**
- * Represents a infinite loop slider
+ * Represents a infinite loop slider.
+ * This component needed for init swiper and he uses
+ * user setting or default if are they present.
  * @constructor
  * @param {ElementRef} elementRef - The element reference of the component
  * @param {any} platformId - The platformId of the server/browser platform
